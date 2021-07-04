f770900477096343b6d63c1c72fcd0e9a9b163d8
Nasmp <nascimentomp3@outlook.com>
2021-07-04 16:38:39 -0300

    classes de dominio finalizadas

--- src/main/java/com/mark1/bookstore/domain/Livro.java
+++ src/main/java/com/mark1/bookstore/domain/Livro.java
@@ -1,16 +1,31 @@
 package com.mark1.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 //import com.sun.beans.util.Cache;
 
-public class Livro {
+@Entity
+public class Livro implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String titulo;
 	private String nome_autor;
 	private String texto;
 
+	@ManyToOne
+	@JoinColumn(name = "categoria_id")
 	private Categoria categoria;
 
 	public Livro() {
