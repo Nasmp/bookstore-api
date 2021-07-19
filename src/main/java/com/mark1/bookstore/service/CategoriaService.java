e0c41e1582e04eac8c9e2e1e622270412401a848
Nasmp <nascimentomp3@outlook.com>
2021-07-19 10:14:40 -0300

    findAll Categoria

--- src/main/java/com/mark1/bookstore/service/CategoriaService.java
+++ src/main/java/com/mark1/bookstore/service/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.mark1.bookstore.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.mark1.bookstore.domain.Categoria;
 import com.mark1.bookstore.repositories.CategoriaRepository;
+import com.mark1.bookstore.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,8 +18,13 @@ public class CategoriaService {
 
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Item não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 
+	}
+	
+	public List<Categoria> findAll(){
+		return repository.findAll();
 	}
 
 }
