d9f1c0de2f9687f8c57d5e129bfa0a28cd574b34
Nasmp <nascimentomp3@outlook.com>
2021-07-19 15:11:36 -0300

    UPDATE Categoria

--- src/main/java/com/mark1/bookstore/service/CategoriaService.java
+++ src/main/java/com/mark1/bookstore/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mark1.bookstore.domain.Categoria;
+import com.mark1.bookstore.dtos.CategoriaDTO;
 import com.mark1.bookstore.repositories.CategoriaRepository;
 import com.mark1.bookstore.service.exceptions.ObjectNotFoundException;
 
@@ -30,5 +31,14 @@ public class CategoriaService {
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
 		return repository.save(obj);
+	}
+
+	public Categoria update(Integer id, CategoriaDTO objDto) {
+		Categoria obj = findById(id);
+		obj.setNome(objDto.getNome());
+		obj.setDescricao(objDto.getDescricao());
+		return repository.save(obj);
+		
+		
 	}
 }
